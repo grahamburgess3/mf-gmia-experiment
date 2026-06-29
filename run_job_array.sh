07109c40851bc97f54b60417c3c2670e39966859
Graham <grahammb_315@hotmail.co.uk>
2026-06-29 22:39:03 +0100

    macro rep 0 for exp050 to exp054

--- run_job_array.sh
+++ run_job_array.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-#SBATCH -J mfgmia-sasena-array-diag
-#SBATCH -o mfgmia-sasena-array-diag_%A_%a.out
-#SBATCH --array=0-9%10
-#SBATCH -c 1
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=1GB
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=g.burgess1@lancaster.ac.uk
-
-# start pyenv
-source ~/start-pyenv
-
-# move into project directory and setup environ
-cd ~/mf-gmia-experiment
-source .venv/bin/activate
-
 # run
-cd ~/mf-gmia-experiment/experiments/exp050
-srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$SLURM_ARRAY_TASK_ID"
+cd ./experiments/exp050
+python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$1"
+cd ../..
+echo "done 50"
 
-cd ~/mf-gmia-experiment/experiments/exp051
-srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$SLURM_ARRAY_TASK_ID"
+cd ./experiments/exp051
+python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$1"
+cd ../..
+echo "done 51"
 
-cd ~/mf-gmia-experiment/experiments/exp052
-srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$SLURM_ARRAY_TASK_ID"
+cd ./experiments/exp052
+python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$1"
+cd ../..
+echo "done 52"
 
-cd ~/mf-gmia-experiment/experiments/exp053
-srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$SLURM_ARRAY_TASK_ID"
+cd ./experiments/exp053
+python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$1"
+cd ../..
+echo "done 53"
 
-cd ~/mf-gmia-experiment/experiments/exp054
-srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$SLURM_ARRAY_TASK_ID"
+cd ./experiments/exp054
+python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --diagnose --macrorep "$1"
+cd ../..
+echo "done 54"
