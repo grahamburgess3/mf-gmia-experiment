bac177ba6d9f1142b1c72fe4a40ad21eda125e67
Graham <grahammb_315@hotmail.co.uk>
2026-06-30 11:06:50 +0100

    new storm script for 100 macroreps of mfgmia-corr

---
+++ run_job_array_storm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -J mfgmia-corr
+#SBATCH -o mfgmia-corr_%A_%a.out
+#SBATCH --array=0-99%50
+#SBATCH -c 1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=1GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=g.burgess1@lancaster.ac.uk
+
+# start pyenv
+source ~/start-pyenv
+
+# move into project directory and setup environ
+cd ~/mf-gmia-experiment
+source .venv/bin/activate
+
+# run
+cd ~/mf-gmia-experiment/experiments/exp053
+srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
