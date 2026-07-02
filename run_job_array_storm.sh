299860e455c1627619f04693b61a8f1ab5f2eb9e
Graham <grahammb_315@hotmail.co.uk>
2026-07-02 17:20:58 +0100

    new job

--- run_job_array_storm.sh
+++ run_job_array_storm.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -J mfgmia-wsc-rev
 #SBATCH -o mfgmia-wsc-rev_%A_%a.out
-#SBATCH --array=0-99%50
+#SBATCH --array=0-999%25
 #SBATCH -c 1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=1GB
@@ -17,5 +17,8 @@ cd ~/mf-gmia-experiment
 source .venv/bin/activate
 
 # run
-cd ~/mf-gmia-experiment/experiments/exp054
+cd ~/mf-gmia-experiment/experiments/exp063
+srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
+
+cd ~/mf-gmia-experiment/experiments/exp064
 srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
