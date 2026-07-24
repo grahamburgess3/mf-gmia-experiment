39d1c335d5c51a2d78aa87890573709aed49bf1a
Graham <grahammb_315@hotmail.co.uk>
2026-07-24 12:17:22 +0100

    new job array

--- run_job_array_storm.sh
+++ run_job_array_storm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH -J mfgmia-wsc-rev
-#SBATCH -o mfgmia-wsc-rev_%A_%a.out
-#SBATCH --array=0-999%25
+#SBATCH -J mfgmia-expl
+#SBATCH -o mfgmia-expl_%A_%a.out
+#SBATCH --array=0-199%50
 #SBATCH -c 1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=1GB
@@ -17,8 +17,17 @@ cd ~/mf-gmia-experiment
 source .venv/bin/activate
 
 # run
-cd ~/mf-gmia-experiment/experiments/exp063
+cd ~/mf-gmia-experiment/experiments/exp066
 srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
 
-cd ~/mf-gmia-experiment/experiments/exp064
+cd ~/mf-gmia-experiment/experiments/exp067
+srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
+
+cd ~/mf-gmia-experiment/experiments/exp068
+srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
+
+cd ~/mf-gmia-experiment/experiments/exp069
+srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
+
+cd ~/mf-gmia-experiment/experiments/exp070
 srun python -m mf_gmia.optimise --config config.yaml --optimise --standardise --parallelise_outside_python --macrorep "$SLURM_ARRAY_TASK_ID" > ./results/results_$SLURM_ARRAY_TASK_ID.txt
